84eb5caff345cb072f89d4cda8abf2dc5c78bf1f
danielecina <icloud.d@gmail.com>
2023-03-30 14:39:36 +0200

    setup parte 2

--- index.js
+++ index.js
@@ -5,22 +5,14 @@ const fs = require('fs-extra')
 const { get, set } = require('lodash')
 
 const fileParser = require('./src/fileParser')
-const { getContributors, getSkillsFromContributors } = require('./src/getContributors')
-const { getProjects } = require('./src/getProjects')
-const { getRowsFromResult } = require('./src/getRowsFromResult')
-const {
-  allocateProjectsInRoadmap,
-  contributorsInProjects,
-  core
-} = require('./src/core')
+const {} = require('./src/core')
 
 const files = [
-  'a_an_example.in.txt',
-  'b_better_start_small.in.txt',
-  'c_collaboration.in.txt',
-  'd_dense_schedule.in.txt',
-  'e_exceptional_skills.in.txt',
-  'f_find_great_mentors.in.txt'
+  'a.txt',
+  // 'b.txt',
+  // 'c.txt',
+  // 'd.txt',
+  // 'e.txt',
 ]
 
 
@@ -29,6 +21,8 @@ function parseFile(fileName) {
     path.join(__dirname, `./input_data/${fileName}`),
     { splitInRows: true }
   )
+
+  console.log('rows', rows)
 
   const [header, ...otherRows] = rows
 
@@ -42,8 +36,8 @@ const start = () => {
     // extract data from file
     const result = parseFile(fileName)
 
-    const rowsList = getRowsFromResult(result)
 
+    const rowsList = []
     // put your core logic here
 
     // save data
